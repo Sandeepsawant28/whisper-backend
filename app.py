51c93d93ace9e8c816212fcef71b02cee5a39b33
Sandeep Sawant <sandeepsawant604@gmail.com>
2026-09-25 15:06:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,9 +58,8 @@ except Exception as e:
 app = Flask(__name__)
 CORS(app)
 
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet",
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="threading",
                      max_http_buffer_size=20_000_000)
-
 streaming_sessions = {}
 
 
